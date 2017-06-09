a41f31751bdeaba66d3f377309b9b208da33ed2d
Dang Rom <phamtandang@gmail.com>
2017-06-09 10:56:43 +0700

    create areas admin

---
+++ src/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DegicEducation.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+    }
+}
