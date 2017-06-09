59c376ee115c034c0e7639b50e72aeae9ada9202
Dang Rom <phamtandang@gmail.com>
2017-06-09 10:52:36 +0700

    create service folder

---
+++ src/Services/IRepository/ICompanyRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DegicEducation.Services.IRepository
+{
+    interface ICompanyRepository
+    {
+    }
+}
---
+++ src/Services/Models/CompanyModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DegicEducation.Services.Models
+{
+    public class CompanyModel
+    {
+    }
+}
